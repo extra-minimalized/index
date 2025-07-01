6385181237d15bf76ee92a79899205e358933017
spaced-design <jaewon4986@naver.com>
2025-07-01 20:50:59 +0900

    first commit

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
-// next.config.js
+const { OutputFileType } = require("typescript");
 
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  output: 'export',
+output : 'export',
   basePath: isProd ? '/index' : '',
   assetPrefix: isProd ? '/index/' : '',
 };
